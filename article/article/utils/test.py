7425b96694fda4ebf545712abae6c02a95a46913
yt <yutaonupt@163.com>
2017-06-20 20:37:45 +0800

    add somethind

--- article/article/utils/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def parse_detail_page(url):
-    html = requests.get(url)
-    print(html.text)
-
-parse_detail_page('https://movie.douban.com/subject/26747853/?tag=%E7%BB%BC%E8%89%BA&from=gaia')
